c3ca78014f9bfcf13fef658db2a5f5cd5a5388c6
beatriz <bia.coelho03@gmail.com>
2024-01-09 11:51:10 -0300

    cursos parcial

---
+++ college/cursos/course.js
@@ -0,0 +1,30 @@
+function AdcCourse(){
+    const btnAdc = document.getElementById('btnAdicionar')
+    btnAdc.onclick = function(){
+        const titleAdded = document.getElementById('title-adc').value;
+        const descriptionAdded = document.getElementById('description-adc').value;
+
+        //const tr = document.createElement('tr');
+        const trTitle = document.getElementById('tr-title')
+        const trDescription = document.getElementById('tr-description')
+
+
+    
+
+        const thTitle = document.createElement('th');
+        thTitle.textContent = titleAdded;
+        trTitle.appendChild(thTitle);
+
+
+        //const trDescription = document.getElementById('tr-description')
+        const tdDescription = document.createElement('td');
+        tdDescription.textContent = descriptionAdded;
+        trDescription.appendChild(tdDescription);
+
+        const table = document.getElementById('table-main');
+        table.appendChild(trDescription, trTitle)
+
+        document.getElementById('title-adc').value = '';
+        document.getElementById('description-adc').value = '';
+    }
+}
